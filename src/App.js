adbb707accdd2f87341f9423065fcaacb0677eb8
wyh760 <woh760@nate.com>
2024-09-11 15:09:56 +0900

    Apply Material Ui

--- src/App.js
+++ src/App.js
@@ -1,91 +1,87 @@
 import React, { Component } from 'react';
 import Customer from './component/Customer';
 import './App.css';
+import { Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material';
+import { withStyles } from '@material-ui/core/styles';
+
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3 ,   // marginTop: theme.spacing(3),  // theme.spacing.unit * 3 대신 사용
+    overflowX: "auto"
+  },
+  table:{
+    minWidth: 1080
+  }
+
+}) 
 
 const customers = [
-    {
-    'id' : 1,
+  {
+    'id': 1,
     'image': 'https://placeimg.com/64/64/any',
-    'name':'John Doe',        
-    'birtday':'1990-01-01',  
-    'gender': 'Male' ,         
-    'job':'Engineer'       
-    },
-    {
-    'id' : 2,
+    'name': 'John Doe',
+    'birtday': '1990-01-01',
+    'gender': 'Male',
+    'job': 'Engineer'
+  },
+  {
+    'id': 2,
     'image': 'https://placeimg.com/66/65/2',
-    'name':'wooyonghak ',       
-    'birtday':'1976-03-21',  
-    'gender': 'girl' ,         
-    'job':'자영업자'        
-    },
-    {
-      'id' : 3,
-      'image': 'https://placeimg.com/66/65/3',
-      'name':'kimheesun ',       
-      'birtday':'1977-07-07',  
-      'gender': 'girl' ,         
-      'job':'자영업자222'        
-    }
-]
+    'name': 'wooyonghak',
+    'birtday': '1976-03-21',
+    'gender': 'girl',
+    'job': '자영업자'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/66/65/3',
+    'name': 'kimheesun',
+    'birtday': '1977-07-07',
+    'gender': 'girl',
+    'job': '자영업자222'
+  }
+];
 
 class App extends Component {
-  render(){
+  render() {
+    const {classes} =this.props; 
     return (
-      <div>
-          {
-            customers.map( c =>{
-                 return(
-                    <Customer 
-                      key={c.id}
-                      id={c.id}
-                      name={c.name}
-                      birtday={c.birtday}
-                      gender={c.gender}
-                      job={c.job}
-                      />
-                 )
-            })
-          }
-      </div>
-    );
+      <Paper className={classes.root}>
+         <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>성함</TableCell>
+              <TableCell>생일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              customers.map(c => {  
+                return(
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birtday={c.birtday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+                )
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
+     );
   }
 }
 
-export default App;
-
-
-      /* <Customer
-         id={customers[0].id}
-         image={customers[0].image}
-         name={customers[0].name}
-         birtday= {customers[0].birtday}
-         gender={customers[0].gender} 
-         job={customers[0].job}
-      />
-      <Customer
-         id={customers[1].id}
-         image={customers[1].image}
-         name={customers[1].name}
-         birtday= {customers[1].birtday}
-         gender={customers[1].gender} 
-         job={customers[1].job}
-      />
-      <Customer
-         id={customers[2].id}
-         image={customers[2].image}
-         name={customers[2].name}
-         birtday= {customers[2].birtday}
-         gender={customers[2].gender} 
-         job={customers[2].job}
-      />
-      </div> */
-       
-
-// function App() {
-//   return (
-//     <Customer/>
-//   );
-// }
-
-// export default App;
+export default withStyles(styles)(App); 
--- src/component/Customer.js
+++ src/component/Customer.js
@@ -1,37 +1,56 @@
 import React from 'react';
+import { Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material'; // 경로 수정
+
 
 class Customer extends React.Component {
     render() {
         return (
-           <div>
-              <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-              <CustomerInfo  birtday={this.props.birtday} gender={this.props.gender}  job={this.props.job} />
-           </div>
-        )
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell>
+                    <img src={this.props.image} alt="profile" style={{ width: '50px', height: '50px' }} />
+                </TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birtday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
+        );
     }
 }
 
-class CustomerProfile extends React.Component{
-    render(){
-        return ( 
-           <div>
-               <img src={this.props.image } alt ="profile" />
-               <h2>{this.props.name}({this.props.id})</h2>
-          </div>
-        )
-    }
-} 
+// class Customer extends React.Component {
+//     render() {
+//         return (
+//            <div>
+//               <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+//               <CustomerInfo  birtday={this.props.birtday} gender={this.props.gender}  job={this.props.job} />
+//            </div>
+//         )
+//     }
+// }
 
-class CustomerInfo extends React.Component{
-    render(){
-        return ( 
-           <div>
-                <p>{this.props.birtday} </p>
-                <p>{this.props.gender} </p>
-                <p>{this.props.job} </p>
-          </div>
-        )
-    }
-} 
+// class CustomerProfile extends React.Component{
+//     render(){
+//         return ( 
+//            <div>
+//                <img src={this.props.image } alt ="profile" />
+//                <h2>{this.props.name}({this.props.id})</h2>
+//           </div>
+//         )
+//     }
+// } 
+
+// class CustomerInfo extends React.Component{
+//     render(){
+//         return ( 
+//            <div>
+//                 <p>{this.props.birtday} </p>
+//                 <p>{this.props.gender} </p>
+//                 <p>{this.props.job} </p>
+//           </div>
+//         )
+//     }
+// } 
 
 export default Customer;
